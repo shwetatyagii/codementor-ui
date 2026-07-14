e142f141b2ee9a81e051dbc84abe70d432c5d064
Shweta Tyagi <shwetatyagi489@gmail.com>
2026-07-15 03:25:16 +0530

    feat: add vercel.json and env var support

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8081",
   headers: {
     "Content-Type": "application/json",
   },
